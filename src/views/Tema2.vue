67c7f200286f530486d1ac8be8dab8e91d848bfc
Andrea Botello <andrea.botellor@misena.edu.co>
2021-09-11 13:09:00 -0700

    updated

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -85,7 +85,7 @@
     //todo! CAMBIAR VIDEO 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/oWDCwu6p9NA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/kkfwi6xJfjQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Animación digital 2D: Photoshop
     //todo! CAMBIAR VIDEO 
     #t_2_3.titulo-segundo
@@ -318,7 +318,7 @@
     //todo! CAMBIAR VIDEO 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/05M0QVroXhI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/oWDCwu6p9NA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Animación digital 2D: After effects
     //todo! CAMBIAR VIDEO 
 </template>
